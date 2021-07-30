f83ae1aa653be5598cd91428b788ef75f1442786
nazeerahmedoffical <nazeerahmedofficial1@gmail.com>
2021-07-30 21:48:04 +0500

    quadritic equation aded

--- java.practise.semster/src/Lab2/Task5_Quadratic_Equation.java
+++ java.practise.semster/src/Lab2/Task5_Quadratic_Equation.java
@@ -1,4 +1,21 @@
 package Lab2;
+import  java.util.Scanner;
+import java.lang.Math;
+public class Task5_Quadratic_Equation
+{
+    public static void main(String args[])
+    {
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter X : ");
+        double x = obj.nextInt();
+        System.out.print("Enter a : ");
+        double a = obj.nextInt();
+        System.out.print("Enter b : ");
+        double b = obj.nextInt();
+        System.out.print("Enter c : ");
+        double c = obj.nextInt();
 
-public class Task5_Quadratic_Equation {
+        double output = (a * Math.pow(x,2)) + (b*x) + c;
+        System.out.println("ax2+bx+c : "+output);
+    }
 }
