9b2bd5d0b363110c83367f241835ca3594533d57
nazeerahmedoffical <nazeerahmedofficial1@gmail.com>
2021-07-30 21:47:14 +0500

    prime number added

--- java.practise.semster/src/Lab2/Task4_primeNumber.java
+++ java.practise.semster/src/Lab2/Task4_primeNumber.java
@@ -1,4 +1,31 @@
 package Lab2;
 
 public class Task4_primeNumber {
+    public static void main (String args[]) {
+        boolean check = true;
+        int [] array = new int[600];
+        int c=0;
+        System.out.print("1 ");
+        for(int i=2;i<600;i++)
+        {
+                for(int j=2;j<i;j++)
+                {
+                    if(i%j==0) {
+                        check = false;
+                        break;
+                    }
+                }
+                if(check==true)
+                {
+                    array[c]=i;
+                    System.out.print(i+" ");
+                    c++;
+                }
+                else {
+                    check = true;
+                }
+
+        }
+
+    }
 }
